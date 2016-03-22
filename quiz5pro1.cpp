b1a844831fcd25c1a39b9e667505ca33633c1889
AlexFooTC <janochivas@hotmail.com>
2016-03-22 11:52:23 -0600

    Create quiz5pro1.cpp

---
+++ quiz5pro1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+bool esPalindromo(string cadena, int size){
+    for(int i = 0; i < size/2; i++)
+        if(cadena[i] != cadena[size-i-1])
+            return false;
+    return true;
+}
+int main(){
+    string cadena;
+    cout << "Ingresa la cadena: ";
+    getline(cin,cadena);
+    esPalindromo(cadena,cadena.length()) ?
+        cout << "\nEs palíndromo \n": cerr << "\nNo es palíndromo \n";
+    return 0;
+}
